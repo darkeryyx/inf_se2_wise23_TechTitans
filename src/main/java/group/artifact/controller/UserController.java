4143e887f9fb3c3eadc1384e0b66f5086f36f5e4
mheppn2s <marcel.heppner@smail.inf.h-brs.de>
2023-11-02 11:33:19 +0100

    registration controller+service

--- src/main/java/group/artifact/controller/UserController.java
+++ src/main/java/group/artifact/controller/UserController.java
@@ -1,8 +1,23 @@
 package group.artifact.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import group.artifact.entities.User;
+import group.artifact.services.UserService;
 
 @RestController
 public class UserController {
+
+    @Autowired
+    UserService service;
     
+    @PostMapping("/register")
+    public void register(@RequestBody User newUser){
+        
+    }
+
 }
+
--- src/main/java/group/artifact/services/UserService.java
+++ src/main/java/group/artifact/services/UserService.java
@@ -1,8 +1,14 @@
 package group.artifact.services;
 
+import group.artifact.entities.User;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    
+    public void createUser(User newUser){
+
+    }
     
 }
