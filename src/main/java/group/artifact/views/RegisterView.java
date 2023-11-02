9e279d23546012d397d3a40707347a4da8979d42
fbiber2s <fabian.biber@smail.inf.h-brs.de>
2023-11-02 15:13:44 +0100

    Eingabe Verifikation & Layout

--- src/main/java/group/artifact/views/RegisterView.java
+++ src/main/java/group/artifact/views/RegisterView.java
@@ -2,44 +2,66 @@ package group.artifact.views;
 
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Composite;
-import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.notification.NotificationVariant;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.router.Route;
+
 
 @Route("register")
 public class RegisterView extends Composite {
 
-    protected Component initContent(){
+    protected Component initContent() {
         EmailField email = new EmailField("E-Mail");
         TextField vorname = new TextField("Vorname");
         TextField nachname = new TextField("Nachname");
         PasswordField passwort = new PasswordField("Passwort");
         PasswordField passwort2 = new PasswordField("Passwort bestätigen");
-        return new VerticalLayout(
-                new H2("Register"),
+        Checkbox checkbox = new Checkbox("Ich stimme den AGBs zu");
+        VerticalLayout layout = new VerticalLayout(
+                new H2("Registrieren"),
                 email,
                 vorname,
                 nachname,
                 passwort,
                 passwort2,
+                checkbox,
                 new Button("Bestätigen", event -> register(
                         email.getValue(),
                         vorname.getValue(),
                         nachname.getValue(),
                         passwort.getValue(),
-                        passwort2.getValue()
+                        passwort2.getValue(),
+                        checkbox.getValue().toString()
                 ))
 
         );
+        layout.setAlignItems(FlexComponent.Alignment.CENTER);
+        return layout;
     }
-    private void register(String email, String vorname, String nachname, String passwort, String passwort2){
-        //hier passiert die register Logik
+
+    private void register(String email, String vorname, String nachname, String passwort, String passwort2, String checkBox) {
+        if (email.trim().isEmpty()) {
+            Notification.show("Bitte eine Email eingeben").addThemeVariants(NotificationVariant.LUMO_ERROR);
+        } else if (vorname.trim().isEmpty()) {
+            Notification.show("Bitte einen Vornamen eingeben").addThemeVariants(NotificationVariant.LUMO_ERROR);
+        } else if (nachname.trim().isEmpty()) {
+            Notification.show("Bitte einen Nachnamen eingeben").addThemeVariants(NotificationVariant.LUMO_ERROR);
+        } else if (!passwort.equals(passwort2)) {
+            Notification.show("Passwörter stimmen nicht überein").addThemeVariants(NotificationVariant.LUMO_ERROR);
+        } else if(checkBox.equals("false")){
+            Notification.show("Bitte stimme unseren AGBs zu").addThemeVariants(NotificationVariant.LUMO_ERROR);
+        } else {
+            Notification.show("Registrierung erfolgreich").addThemeVariants(NotificationVariant.LUMO_SUCCESS);
+            //authService.register(email, vorname, nachname, passwort)
+        }
     }
 }
