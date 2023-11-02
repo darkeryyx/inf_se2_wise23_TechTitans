e8acdf97aaf1c9dc417a3aada26e7e2ad0482fa7
Richi <richi@hansehart.de>
2023-11-02 18:35:20 +0100

    removed: restcontroller

--- src/main/java/group/artifact/controller/UserController.java
+++ src/main/java/group/artifact/controller/UserController.java
@@ -2,27 +2,21 @@ package group.artifact.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import group.artifact.entities.User;
 import group.artifact.services.UserService;
 
-@RestController
-@RequestMapping("/api")
 public class UserController {
 
     @Autowired
     UserService service;
 
-    @PostMapping("/register")
-    public void register(@RequestBody User newUser) {
+    public void register( User newUser) {
         service.createUser(newUser);
     }
 
     @PostMapping("/login")
-    public void login(@RequestBody User user) {
+    public void login(User user) {
         // leeets gooo
     }
 }
