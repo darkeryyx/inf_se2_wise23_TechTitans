be7a5e8eeafdb12d5b1462691fa1fe3521947fee
Richi <richi@hansehart.de>
2023-11-02 11:50:10 +0100

    added: isUnique for Users

--- src/main/java/group/artifact/repositories/UserRepository.java
+++ src/main/java/group/artifact/repositories/UserRepository.java
@@ -1,9 +1,13 @@
 package group.artifact.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import group.artifact.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-    
+    // check if first method parameter (?1 = String name) exists, count the
+    // occurance and check if its 1
+    @Query("SELECT COUNT(u) = 0 FROM User u WHERE u.username = ?1")
+    boolean isUnique(String name);
 }
