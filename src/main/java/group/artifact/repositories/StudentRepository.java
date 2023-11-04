e4d26b04833a5a93fe1f4fdffd9cedc2ae8ec344
Lionel <lionel.schmidt@gmx.de>
2023-11-05 00:49:51 +0100

    Add Query to getReferenceById StudenRepository

--- src/main/java/group/artifact/repositories/StudentRepository.java
+++ src/main/java/group/artifact/repositories/StudentRepository.java
@@ -5,4 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentRepository extends JpaRepository<Student, Integer> {
 
+    @Query("SELECT s FROM Student S WHERE s.student_pk = ?1")
+    Student getReferenceById(Integer id);
+
 }
