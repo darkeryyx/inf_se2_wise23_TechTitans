f199ac8d30ff846b13388d919100710ee49d062d
Lukas <lukas.hoerdemann@smail.inf.h-brs.de>
2023-11-04 16:33:42 +0100

    added method for creating student profiles

--- src/main/java/group/artifact/controller/StudentController.java
+++ src/main/java/group/artifact/controller/StudentController.java
@@ -1,4 +1,28 @@
 package group.artifact.controller;
 
+
+import group.artifact.entities.Student;
+import group.artifact.services.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
 public class StudentController {
+
+    @Autowired
+    private StudentService service;
+
+
+    public Student viewStudentProfile(Integer id) {
+        return service.viewProfile(id);
+    }
+
+
+    public void createStudentProfile(Student student) {
+        service.saveProfile(student);
+    }
 }
--- src/main/java/group/artifact/services/StudentService.java
+++ src/main/java/group/artifact/services/StudentService.java
@@ -1,4 +1,23 @@
 package group.artifact.services;
 
+import group.artifact.entities.Student;
+import group.artifact.entities.User;
+import group.artifact.repositories.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class StudentService {
+
+    @Autowired
+    StudentRepository repo;
+    public void saveProfile(Student student) {
+        repo.save(student);
+    }
+
+    public Student viewProfile(Integer id) {
+        return repo.getReferenceById(id);
+    }
 }
