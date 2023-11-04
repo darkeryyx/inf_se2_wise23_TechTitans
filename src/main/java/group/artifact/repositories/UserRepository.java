4a83a6fa370f79b576ba08bc2589dd7aee64eef4
Sam Taheri <sam.taheri@smail.inf.h-brs.de>
2023-11-04 20:45:01 +0100

    give the name,surname,password,salt back from the database

--- src/main/java/group/artifact/repositories/UserRepository.java
+++ src/main/java/group/artifact/repositories/UserRepository.java
@@ -1,8 +1,6 @@
 package group.artifact.repositories;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
 import group.artifact.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
@@ -10,4 +8,25 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     // occurance and check if its 1
     @Query("SELECT COUNT(u) = 0 FROM User u WHERE u.email = ?1")
     boolean isEmailUnique(String email);
+
+    // SELECT surname
+    // FROM User u
+    // WHERE u.surname = [StringValueOf(surname)] AND u.name = [StringValueOf(name)]
+    @Query("SELECT u FROM User u WHERE u.surname LIKE ?1 AND u.name LIKE ?2")
+    User findUserBySurnameAndName ( String surname , String name);
+
+    // SELECT password
+    // FROM User u
+    // WHERE u.password LIKE ?1 AND u.name LIKE ?2
+    @Query("SELECT u.password FROM User u WHERE u.surname LIKE ?1 AND u.name LIKE ?2")
+    String findUserByPassword (String surname, String name);
+
+    // SELECT u.salt
+    // FROM user u
+    // WHERE u.surname LIKE ?1 AND u.name LIKE ?2
+    @Query("SELECT u.salt FROM User u WHERE u.surname LIKE ?1 AND u.name LIKE ?2")
+    String findSaltBySurnameAndName(String surname, String name);
+
+
+
 }
