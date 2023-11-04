883e27f3cbb0f2b26bcf443134e772e47db7d198
Lukas <lukas.hoerdemann@smail.inf.h-brs.de>
2023-11-04 16:34:21 +0100

    method for saving company profiles

--- src/main/java/group/artifact/controller/CompanyController.java
+++ src/main/java/group/artifact/controller/CompanyController.java
@@ -1,13 +1,21 @@
 package group.artifact.controller;
 
+import group.artifact.entities.Company;
+import group.artifact.entities.Student;
+import group.artifact.services.CompanyService;
+import group.artifact.services.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CompanyController {
 
-    @GetMapping("/company")
-    public void viewCompany(){
+    @Autowired
+    CompanyService service;
 
+    public void createCompany(Company company){
+        service.saveProfile(company);
     }
 }
