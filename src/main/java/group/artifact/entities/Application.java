bc5cb3f45c70cd1a58879bdafbdf956cfba60231
Richi <richi@hansehart.de>
2023-11-05 19:42:19 +0100

    added: application as entity

--- src/main/java/group/artifact/entities/Application.java
+++ src/main/java/group/artifact/entities/Application.java
@@ -3,12 +3,11 @@ package group.artifact.entities;
 import java.time.ZonedDateTime;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -17,18 +16,19 @@ import lombok.Data;
 @Entity
 @Table(name = "applications", schema = "project")
 public class Application {
-    // primary keys
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer application_pk;
+    // primary keys (composite)
+    @EmbeddedId
+    private ApplicationCompositeKey application_pk;
 
     // foreign keys
     @ManyToOne
-    @JoinColumn(name = "student_pk", nullable = false)
+    @MapsId("student_pk") // in ApplicationCompositeKey
+    @JoinColumn(name = "student_pk", nullable = false) // in Student
     private Student student;
 
     @ManyToOne
-    @JoinColumn(name = "offer_pk", nullable = false)
+    @MapsId("offer_pk") // in ApplicationCompositeKey
+    @JoinColumn(name = "offer_pk", nullable = false) // in Offer
     private Offer offer;
 
     // attributes
