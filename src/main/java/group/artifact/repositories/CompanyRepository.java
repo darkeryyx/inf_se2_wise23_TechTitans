b5a271b09471454d96e9a33d256a920422cfdfce
Lionel <lionel.schmidt@gmx.de>
2023-11-05 01:17:02 +0100

    Add Query to getReferenceById

--- src/main/java/group/artifact/repositories/CompanyRepository.java
+++ src/main/java/group/artifact/repositories/CompanyRepository.java
@@ -1,7 +1,12 @@
 package group.artifact.repositories;
 
 import group.artifact.entities.Company;
+import group.artifact.entities.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CompanyRepository extends JpaRepository<Company, Integer> {
+
+    @Query("SELECT c FROM Company c WHERE c.company_pk = ?1")
+    Company getReferenceById(Integer id);
 }
--- src/main/java/group/artifact/repositories/StudentRepository.java
+++ src/main/java/group/artifact/repositories/StudentRepository.java
@@ -2,6 +2,8 @@ package group.artifact.repositories;
 
 import group.artifact.entities.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 
 public interface StudentRepository extends JpaRepository<Student, Integer> {
 
