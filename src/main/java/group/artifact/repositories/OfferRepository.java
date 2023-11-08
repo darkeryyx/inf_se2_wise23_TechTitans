47680ab17b0fef2256662a5e728424d2ec5a5262
robin <robin.broich@smail.inf.h-brs.de>
2023-11-08 14:00:24 +0100

    OfferRepository

---
+++ src/main/java/group/artifact/repositories/OfferRepository.java
@@ -0,0 +1,12 @@
+package group.artifact.repositories;
+
+import group.artifact.entities.Offer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
+
+public interface OfferRepository extends JpaRepository<Offer, Integer> {
+    @Query(" SELECT o.description, o.business, o.income, o.job, o.company.name" +
+            " FROM Offer o" )
+    List<Object[]> findAllOffersAndTheirCompany();
+}
