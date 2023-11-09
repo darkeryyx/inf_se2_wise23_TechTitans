7dd011f4111e22d3923b4d14695081cf6f297966
Melissa <melissa.querbach@smail.inf.h-brs.de>
2023-11-09 14:27:07 +0100

    added: query for the attributes needed for displaying companies for the search

--- src/main/java/group/artifact/repositories/CompanyRepository.java
+++ src/main/java/group/artifact/repositories/CompanyRepository.java
@@ -1,7 +1,13 @@
 package group.artifact.repositories;
 
 import group.artifact.entities.Company;
+import group.artifact.dtos.CompanyDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 
 public interface CompanyRepository extends JpaRepository<Company, Integer> {
+
+    @Query("SELECT c.image, c.name, c.link FROM Company c")
+    List<CompanyDTO> findAllCompanies();
 }
