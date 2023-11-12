cbf162c65cafabcff5af5a38d0f6a0452ed757df
Lukas <lukas.hoerdemann@smail.inf.h-brs.de>
2023-11-12 15:22:41 +0100

    changed: type of semester to Integer

--- src/main/java/group/artifact/dtos/impl/StudentDTOImpl.java
+++ src/main/java/group/artifact/dtos/impl/StudentDTOImpl.java
@@ -2,17 +2,21 @@ package group.artifact.dtos.impl;
 
 import group.artifact.dtos.StudentDTO;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Getter
+@Setter
+@NoArgsConstructor
 public class StudentDTOImpl implements StudentDTO {
     private String name;
     private String surname;
     private String email;
     private String subject; // academic subject
     private LocalDate birthday;
-    private Short semester;
+    private Integer semester;
     private String skills;
     private String interests;
     private String description;
