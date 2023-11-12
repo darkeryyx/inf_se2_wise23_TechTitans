b5fa42c96282f37b2130b99625c1988a6d0c91f0
Lukas <lukas.hoerdemann@smail.inf.h-brs.de>
2023-11-12 15:20:42 +0100

    added: ModelMapper Bean

--- src/main/java/group/artifact/ArtifactApplication.java
+++ src/main/java/group/artifact/ArtifactApplication.java
@@ -1,11 +1,16 @@
 package group.artifact;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ArtifactApplication {
-
+	@Bean
+	public ModelMapper modelMapper()  {
+		return new ModelMapper();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ArtifactApplication.class, args);
 	}
