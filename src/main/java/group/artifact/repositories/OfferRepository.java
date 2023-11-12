13c02ecaef486bdde82c33069deee57e45966980
Melissa <melissa.querbach@smail.inf.h-brs.de>
2023-11-12 16:28:10 +0100

    changed: return type of the functions changed from list of Object[] to list of OfferDTOs

--- src/main/java/group/artifact/repositories/OfferRepository.java
+++ src/main/java/group/artifact/repositories/OfferRepository.java
@@ -1,16 +1,19 @@
 package group.artifact.repositories;
 
 import group.artifact.entities.Offer;
+import group.artifact.dtos.OfferDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface OfferRepository extends JpaRepository<Offer, Integer> {
-    @Query(" SELECT o.description, o.business, o.income, o.job, o.company.name" +
+
+    //TODO: Attribute festlegen
+    @Query(" SELECT o.company.image, o.company.name, o.business, o.income, o.job" +
             " FROM Offer o" )
-    List<Object[]> findAllOffersAndTheirCompany();
+    List<OfferDTO> findAllOffersAndTheirCompany();
 
     @Query(" SELECT DISTINCT o.business" +
             " FROM Offer o" )
-    List<Object[]> findAllbusinesses();
+    List<OfferDTO[]> findAllbusinesses();
 }
