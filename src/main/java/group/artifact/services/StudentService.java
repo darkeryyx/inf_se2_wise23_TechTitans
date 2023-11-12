69d91729453f779df950ea1baaf6717b78169c6e
Lukas <lukas.hoerdemann@smail.inf.h-brs.de>
2023-11-12 15:23:44 +0100

    added: method to map Student and User to StudentDTO

--- src/main/java/group/artifact/services/StudentService.java
+++ src/main/java/group/artifact/services/StudentService.java
@@ -1,5 +1,7 @@
 package group.artifact.services;
 
+import group.artifact.dtos.StudentDTO;
+import group.artifact.dtos.impl.StudentDTOImpl;
 import group.artifact.entities.Student;
 import group.artifact.entities.User;
 import group.artifact.repositories.StudentRepository;
@@ -10,6 +12,8 @@ import lombok.AllArgsConstructor;
 
 import java.util.Optional;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,6 +24,7 @@ public class StudentService {
     private StudentRepository studentRepository;
     private UserRepository userRepository;
     private EntityManager entityManager;
+    private ModelMapper mapper;
 
     @Transactional
     public void saveProfile(Student student, Integer userId) {
@@ -31,8 +36,15 @@ public class StudentService {
         }
     }
 
-    public Student viewProfile(Integer id) {
-        return studentRepository.findById(id)
+    public StudentDTO viewProfile(Integer id) {
+         Student student = studentRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Student not found with id " + id));
+         return mapToDTO(student);
+    }
+
+    private StudentDTO mapToDTO(Student student) {
+        StudentDTO studentDTO = mapper.map(student, StudentDTOImpl.class);
+        mapper.map(student.getUser(),studentDTO);
+        return studentDTO;
     }
 }
