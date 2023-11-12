eab4a66321b82fd25dbbcb6f6d61a4d55e92f210
Melissa <melissa.querbach@smail.inf.h-brs.de>
2023-11-12 16:14:47 +0100

    created: OfferDTO

---
+++ src/main/java/group/artifact/dtos/OfferDTO.java
@@ -0,0 +1,10 @@
+package group.artifact.dtos;
+
+import group.artifact.entities.Company;
+
+public interface OfferDTO {
+
+    public String getLogo();
+    public String getNameOfCompany();
+    public String getIncome();
+}
