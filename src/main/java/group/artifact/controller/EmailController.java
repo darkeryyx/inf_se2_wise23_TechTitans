f46317a7238c80bdf22130719730f15166cb5ae5
Richi <richi@hansehart.de>
2023-11-21 19:47:17 +0100

    added: email verification

---
+++ src/main/java/group/artifact/controller/EmailController.java
@@ -0,0 +1,16 @@
+package group.artifact.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import group.artifact.services.EmailService;
+
+@Component
+public class EmailController {
+    @Autowired
+    EmailService emailService;
+
+    public void sendEmail(String adress, String subject, String text) {
+        emailService.sendEmail(adress, subject, text);
+    }
+}
