8e88d5b69460e0317dbb9a1b67fe1055a8a3c05b
Bertrand Alougou <bertrand.alougou@yahoo.com>
2023-11-27 13:27:09 +0100

    CreateCompanyProfileView update

--- src/main/java/group/artifact/views/CreateCompanyProfileView.java
+++ src/main/java/group/artifact/views/CreateCompanyProfileView.java
@@ -1,5 +1,10 @@
 package group.artifact.views;
 
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.BoxSizing;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.IntegerField;
 import com.vaadin.flow.component.textfield.TextArea;
@@ -45,6 +50,9 @@ public class CreateCompanyProfileView extends Composite<Component> {
         var description = createTextArea("Beschreibung");
         HorizontalLayout buttonLayout = new HorizontalLayout();
 
+        Div container = new Div(); // Container to hold layout
+        container.setWidth("100%"); // Full width of the page
+
 
         //create submit button
         Button createProfileButton = new Button("Bestätigen", event -> createCompanyProfile(
@@ -57,18 +65,38 @@ public class CreateCompanyProfileView extends Composite<Component> {
                 logo.getValue(),
                 description.getValue()
                 ));
+        createProfileButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
         createProfileButton.addClickListener(e -> Notification.show("Unternehmen" + name.getValue() + "erfolgreich angelegt"));
 
         //create cancel button. button goes back to Log in view
+
         Button cancelButton = new Button("Abbrechen", e -> {
             getUI().ifPresent(ui -> ui.navigate(LoginView.class));  //when user is present navigate back to log in view
             Notification.show("Firmenprofilerstellung abgebrochen!");
         });
 
-        buttonLayout.add(createProfileButton, cancelButton);
+
+        // Add spacing between buttons
         buttonLayout.setSpacing(true);
 
+        //SetWidthfull
+        buttonLayout.setWidthFull();
 
+        // Adjust alignment to center
+        buttonLayout.setAlignItems(FlexComponent.Alignment.CENTER);
+
+        buttonLayout.setJustifyContentMode(FlexComponent.JustifyContentMode.BETWEEN);
+
+        // Increase button size for better visibility
+        createProfileButton.setWidth("200px");
+        cancelButton.setWidth("200px");
+
+
+        // Add buttons to the layout
+        buttonLayout.add(createProfileButton, cancelButton);
+
+/**
+ * //Vertical layout v1
         VerticalLayout layout = new VerticalLayout(
                 new H2("Firmenprofil anlegen"),
                 user,
@@ -80,12 +108,51 @@ public class CreateCompanyProfileView extends Composite<Component> {
                 logo,
                 description,
                 buttonLayout);
+ **/
+
+        //create vertical layout which is the main layout of the view
+        VerticalLayout layout = new VerticalLayout(
+                new H2("Firmenprofil anlegen"),
+                createFormLayout(user, name, business, employees, founded, link, logo, description),
+                buttonLayout
+        );
         if (business.isInvalid() | link.isInvalid() | description.isInvalid())
             Notification.show("Bitte füllen Sie das erforderliche Feld aus.");
+        // Make the layout more compact
+        layout.setPadding(true);
+
+        // Center the layout
         layout.setAlignItems(FlexComponent.Alignment.CENTER);
-        layout.setSizeFull();
-        return layout;
+        layout.setHorizontalComponentAlignment(FlexComponent.Alignment.CENTER, buttonLayout);
+
+        // Set a fixed width or maximum width to control the layout size
+        layout.setMaxWidth("80em");
+        //layout.setMargin(true);
+
+        // Adjust the width of the form components for better responsiveness
+        user.setWidthFull();
+        name.setWidthFull();
+        business.setWidthFull();
+        employees.setWidthFull();
+        founded.setWidthFull();
+        link.setWidthFull();
+        logo.setWidthFull();
+
+
+        //add layout to container
+        container.add(layout);
+
+        // Center the container on the page
+        container.getStyle().set("margin", "auto");
+        container.getStyle().set("display", "flex");
+        container.getStyle().set("flexDirection", "column");
+        container.getStyle().set("alignItems", "center");
+
+
+
+        return container;
     }
+
 
     private void createCompanyProfile(Integer user, String name, String business, Integer employees, LocalDate founded, String link,
             String description, String logo) {
@@ -126,7 +193,7 @@ public class CreateCompanyProfileView extends Composite<Component> {
         TextArea textArea = new TextArea(label);
         textArea.setHelperText("beschreiben Sie Ihr Unternehmen kurz");
         textArea.setPlaceholder("Schreiben Sie hier . . . ");
-        textArea.setWidth("30%");
+        textArea.setWidth("80%");
         textArea.setHeight("30%");
         textArea.setClearButtonVisible(true);
 
@@ -142,6 +209,17 @@ public class CreateCompanyProfileView extends Composite<Component> {
         integerField.setWidth("10%");
         integerField.setStepButtonsVisible(true);
         return integerField;
+    }
+
+    private FormLayout createFormLayout(TextField user, TextField name, TextField business, IntegerField employees, DatePicker founded, TextField link, TextField logo, TextArea description) {
+        FormLayout formLayout = new FormLayout();
+        formLayout.add(user, name, business, employees, founded, link, logo, description);
+        formLayout.setResponsiveSteps(
+                new FormLayout.ResponsiveStep("0", 1),
+                new FormLayout.ResponsiveStep("21em", 2),
+                new FormLayout.ResponsiveStep("32em", 3)
+        );
+        return formLayout;
     }
 
     private void showSuccessNotification() {
